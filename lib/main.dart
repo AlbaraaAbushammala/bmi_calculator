da0c92234cb848888dd48dd961218d8fb9149952
baraa-abushammala <baraaabushammala1991@gmail.com>
2021-11-17 20:05:14 +0100

    >>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bmi_calculator/bmi_result.dart';
+
 import 'package:bmi_calculator/bmi_screen.dart';
 import 'package:bmi_calculator/home_screen.dart';
 import 'package:bmi_calculator/info_screen.dart';
@@ -9,7 +10,7 @@ void main() {
   runApp(MyApp());
 }
 
-// heloooo ali
+//
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
